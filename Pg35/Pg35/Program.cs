3e7c4084c286a795f0b9d8a84eee98970a2d89e4
Sara Hanberg <sarahanberg69@gmail.com>
2018-04-26 19:35:37 -0700

    Add files via upload

---
+++ Pg35/Pg35/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me a number between 1 and 100.  Whole numbers only, please.");
+            int choice = Convert.ToInt32(Console.ReadLine());
+        }
+        // Not sure if thi line is what I need
+        answerpg35 num1 = new answerpg35();
+        
+
+
+
+
+    }
+
+}
---
+++ Pg35/Pg35/answerpg35.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg35
+{
+    public class answerpg35
+    {
+        public answerpg35()
+        {
+
+        }
+
+        public int answer1 { get; set; }
+        public int answer2 { get; set; }
+        public int answer3 { get; set; }
+
+        
+        
+    }
+}
