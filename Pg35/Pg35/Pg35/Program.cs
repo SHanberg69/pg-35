9d0415ede5a7c4743fd8fc78a206530d59e0a6bf
Sara Hanberg <sarahanberg69@gmail.com>
2018-05-03 18:43:05 -0700

    Add files via upload

---
+++ Pg35/Pg35/Pg35/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg35
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please give me a number between 1 and 100.  Whole numbers only, please.");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(choice + " multiplied by 7 equals " + answerpg35.MathOne(choice));
+            Console.WriteLine(choice + " plus 100 equals " + answerpg35.MathTwo(choice));
+            Console.WriteLine(choice + " divided by 2 equals " + answerpg35.MathThree(choice));
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
---
+++ Pg35/Pg35/Pg35/answerpg35.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1. Create a class. In that class, create three methods, each of which will take one integer parameter in and 
+//return an integer.The methods should do some math operation on the received parameter.
+//                                Put this class in a separate.cs file in the application.
+
+//2. In the Main() program, ask the user what number they want to do the math operations on.
+
+//3. Call each method in turn, passing the user input to the method.Display the returned integer to the screen.
+
+namespace Pg35
+{
+    public class answerpg35
+    {
+   
+        
+            public static int MathOne (int choice)
+            {
+                int result1 = choice * 7;
+                return result1;
+            }
+
+            public static int MathTwo(int choice)
+            {
+                int result2 = choice + 100;
+                return result2;
+            }
+
+            public static int MathThree(int choice)
+            {
+                int result3 = choice / 2;
+                return result3;
+            }
+
+
+
+
+
+
+    }
+}
