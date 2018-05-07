d607734e505343b7452e1c8b15b6f82426fcf8ff
Sara Hanberg <sarahanberg69@gmail.com>
2018-05-06 17:09:20 -0700

    Add files via upload

--- Pg35/Pg35/Program.cs
+++ Pg35/Pg35/Program.cs
@@ -6,22 +6,27 @@ using System.Threading.Tasks;
 
 namespace Pg35
 {
-    class Program
+    public class Program
     {
-        static int Main(int[] args)
+        static void Main()
         {
             Console.WriteLine("Please give me a number between 1 and 100.  Whole numbers only, please.");
             int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(choice + " multiplied by 7 equals " + answerpg35.MathOne(choice));
+            Console.WriteLine(choice + " plus 100 equals " + answerpg35.MathTwo(choice));
+            Console.WriteLine(choice + " divided by 2 equals " + answerpg35.MathThree(choice));
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
         }
-
-        Console.Writeline(choice + " multiplied by 7 equals " + MathOne);
-
-        
-        
-
-
-
-
     }
 
 }
